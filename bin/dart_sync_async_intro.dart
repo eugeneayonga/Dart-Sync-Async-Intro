6ff8e10733496093a5ff783e40e7f6d251b03746
Eugene <eugeneayonga@gmail.com>
2023-05-11 10:58:42 +0300

    First commit

---
+++ bin/dart_sync_async_intro.dart
@@ -0,0 +1,5 @@
+import 'package:dart_sync_async_intro/dart_sync_async_intro.dart' as dart_sync_async_intro;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_sync_async_intro.calculate()}!');
+}
